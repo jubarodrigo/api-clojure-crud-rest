1d0f4ad97cbc9b7a9622eb76e01c0625ebd4fbae
jubarodrigo <rodrigo.gomes@ntk.com.br>
2019-08-09 17:04:25 -0300

    app

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject todo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-json "0.5.0"]
+                 [korma "0.3.0-RC5"]
+                 [mysql/mysql-connector-java "5.1.6"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler todo.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
